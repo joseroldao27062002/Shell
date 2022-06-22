0501e42bdf74d9add6d4d38578f298dadfa102c2
joseroldao27062002 <roldao27602@gmail.com>
2022-06-21 22:43:07 -0300

    Delete

--- projetoSO/testeScript.sh
+++
@@ -1,5 +0,0 @@
-#Script para o teste do tempo execução, tempo de memoria, kernel e usuário
-for ((i=0;i<50;i++))
-do
-	//usr/bin/time --format='tempo=%E memoria=%M kernel=%S usr=%U' ./script.sh
-done
